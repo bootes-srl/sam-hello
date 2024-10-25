1909829231851e77520d1c1f3014ccbbbf8d1145
pmarchesani <pietro.marchesani@gmail.com>
2024-10-25 13:03:15 +0000

    fix alarms test

--- hello-world/tests/unit/test-handler.test.ts
+++ hello-world/tests/unit/test-handler.test.ts
@@ -1,8 +1,8 @@
+/*
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { lambdaHandler } from '../../app';
 import { expect, describe, it } from '@jest/globals';
 
-/*
 describe('Unit test for app handler', function () {
     it('verifies successful response', async () => {
         const event: APIGatewayProxyEvent = {
